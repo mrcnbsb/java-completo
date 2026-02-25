c52285ef40c56e6b2f1c315c454d71a1c1ad4bf7
mrcnbsb <marconebsb@gmail.com>
2026-02-25 10:48:49 -0300

    User entity and resource

---
+++ course_spring/src/main/java/com/cursojavacompleto/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.cursojavacompleto.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursojavacompleto.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
