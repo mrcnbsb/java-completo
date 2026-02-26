c573e17ee23468412534371dc2538a03f7b49eb4
mrcnbsb <marconebsb@gmail.com>
2026-02-26 09:41:32 -0300

    Service layer, component registration

--- course_spring/src/main/java/com/cursojavacompleto/course/repositories/UserRepository.java
+++ course_spring/src/main/java/com/cursojavacompleto/course/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.cursojavacompleto.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cursojavacompleto.course.entities.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
