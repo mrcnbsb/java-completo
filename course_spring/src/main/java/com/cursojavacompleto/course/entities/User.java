6e20ccf660bfcdf9c39648d45ea84a53c0a4cb42
mrcnbsb <marconebsb@gmail.com>
2026-03-02 21:47:46 -0300

    Payment, onte-to-one association

--- course_spring/src/main/java/com/cursojavacompleto/course/entities/User.java
+++ course_spring/src/main/java/com/cursojavacompleto/course/entities/User.java
@@ -17,6 +17,8 @@ import jakarta.persistence.Table;
 @Table(name = "tb_user")
 public class User implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
