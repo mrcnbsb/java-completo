0f99dc99cd6494d1c944807e934e67d3c65cd2e3
mrcnbsb <marconebsb@gmail.com>
2026-03-09 21:31:34 -0300

    Insercao de usuario com POST

--- course_spring_mongoDB/workshopmongo/src/main/java/com/marconeamaral/workshopmongo/resources/UserResource.java
+++ course_spring_mongoDB/workshopmongo/src/main/java/com/marconeamaral/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.marconeamaral.workshopmongo.resources;
 
+import java.net.URI;
 //import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,10 +8,12 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marconeamaral.workshopmongo.domain.User;
 import com.marconeamaral.workshopmongo.dto.UserDTO;
@@ -42,11 +45,22 @@ public class UserResource { //controller
         return ResponseEntity.ok().body(listDto); //ok() é um método estático que retorna um ResponseEntity com status HTTP 200 (OK). body(list) é um método que define o corpo da resposta como a lista de usuários.
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET) //pra informar que esse método responde a requisições do tipo GET
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET) //pra informar que esse método responde a requisições do tipo GET e que o caminho de acesso a esse recurso é /users/{id}, onde {id} é um parâmetro que representa o ID do usuário a ser buscado.
+    //@GetMapping(value = "/{id}") //outra forma de informar que esse método responde a requisições do tipo GET e que o caminho de acesso a esse recurso é /users
     public ResponseEntity<UserDTO> findById(@PathVariable String id) { //@PathVariable é uma anotação do Spring que indica que o valor do parâmetro id deve ser extraído da URL da requisição. Por exemplo, se a URL for /users/123, o valor "123" será atribuído ao parâmetro id.
        User obj = service.findById(id); //findById() é um método do UserService que retorna um usuário com base no ID fornecido. Ele provavelmente chama o método findById() do UserRepository para buscar o dado no MongoDB. O resultado é armazenado na variável obj do tipo User.
        return ResponseEntity.ok().body(new UserDTO(obj)); //ok() é um método estático que retorna um ResponseEntity com status HTTP 200 (OK). body(new UserDTO(obj)) é um método que define o corpo da resposta como um novo objeto do tipo UserDTO, criado a partir do objeto User retornado pelo serviço.        
     }
+
+    @RequestMapping(method = RequestMethod.POST) //pra informar que esse método responde a requisições do tipo POST e que o caminho de acesso a esse recurso é /users, onde {id} é um parâmetro que representa o ID do usuário a ser buscado.
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) { //@RequestBody é uma anotação do Spring que indica que o corpo da requisição deve ser convertido em um objeto do tipo UserDTO. O objeto UserDTO será criado a partir dos dados enviados no corpo da requisição.
+        User obj = service.fromDTO(objDto); //fromDTO() é um método do UserService que converte um objeto do tipo UserDTO em um objeto do tipo User. Ele recebe um UserDTO como argumento e retorna um novo objeto User, usando os dados do UserDTO para preencher os campos correspondentes do User.
+        obj = service.insert(obj); //insert() é um método do UserService que insere um novo usuário no banco de dados. Ele provavelmente chama o método insert() do UserRepository para salvar
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); //ServletUriComponentsBuilder é uma classe do Spring que ajuda a construir URIs de forma fluente. fromCurrentRequest() cria um builder a partir da URI da requisição atual. path("/{id}") adiciona um segmento de caminho com um parâmetro {id}. buildAndExpand(obj.getId()) substitui o parâmetro {id} pelo valor do ID do objeto inserido. toUri() converte o builder em um objeto URI.
+        return ResponseEntity.created(uri).build(); //created(uri) é um método estático que retorna um ResponseEntity com status HTTP 201 (Created) e um header "Location" contendo a URI do recurso criado. build() é um método que finaliza a construção do ResponseEntity e o retorna como resposta da requisição.
+    }
+
+
         
 
 }
--- course_spring_mongoDB/workshopmongo/src/main/java/com/marconeamaral/workshopmongo/services/UserService.java
+++ course_spring_mongoDB/workshopmongo/src/main/java/com/marconeamaral/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marconeamaral.workshopmongo.domain.User;
+import com.marconeamaral.workshopmongo.dto.UserDTO;
 import com.marconeamaral.workshopmongo.repository.UserRepository;
 import com.marconeamaral.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,13 @@ public class UserService {
        Optional<User> obj = repo.findById(id); //findById() é um método do MongoRepository que retorna um Optional contendo o documento correspondente ao ID fornecido. O Optional é uma classe que pode conter um valor ou estar vazio, e é usada para evitar problemas de null pointer.
        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); //orElseThrow() é um método do Optional que retorna o valor contido no Optional se ele estiver
     }
+
+    public User insert(User obj) {
+        return repo.insert(obj); //insert() é um método do MongoRepository que insere um novo documento no MongoDB. Ele é usado para criar um novo registro na coleção correspondente à entidade User. O objeto User passado como argumento será salvo no banco de dados e o método retornará o objeto inserido, incluindo o ID gerado pelo MongoDB.
+    }
         
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail()); //fromDTO() é um método que converte um objeto do tipo UserDTO em um objeto do tipo User. Ele recebe um UserDTO como argumento e retorna um novo objeto User, usando os dados do UserDTO para preencher os campos correspondentes do User.
+    }
 
 }
